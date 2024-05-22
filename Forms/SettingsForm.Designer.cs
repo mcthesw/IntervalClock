4554c02b7b962a8cf2a95ed119736e5cfeb4f51f
Callay <2415993100@qq.com>
2024-05-22 23:15:44 +0800

    SettingsForm添加控件自适应窗口大小

--- Forms/SettingsForm.Designer.cs
+++ Forms/SettingsForm.Designer.cs
@@ -45,17 +45,19 @@
             // ShowClockLabel
             // 
             ShowClockLabel.AutoSize = true;
-            ShowClockLabel.Location = new Point(22, 18);
+            ShowClockLabel.Location = new Point(17, 15);
+            ShowClockLabel.Margin = new Padding(2, 0, 2, 0);
             ShowClockLabel.Name = "ShowClockLabel";
-            ShowClockLabel.Size = new Size(69, 20);
+            ShowClockLabel.Size = new Size(56, 17);
             ShowClockLabel.TabIndex = 0;
             ShowClockLabel.Text = "显示时钟";
             // 
             // ShowClockButton
             // 
-            ShowClockButton.Location = new Point(168, 14);
+            ShowClockButton.Location = new Point(131, 12);
+            ShowClockButton.Margin = new Padding(2, 3, 2, 3);
             ShowClockButton.Name = "ShowClockButton";
-            ShowClockButton.Size = new Size(94, 29);
+            ShowClockButton.Size = new Size(73, 25);
             ShowClockButton.TabIndex = 1;
             ShowClockButton.Text = "显示";
             ShowClockButton.UseVisualStyleBackColor = true;
@@ -63,46 +65,51 @@
             // 
             // ShowClockTimeNumSelect
             // 
-            ShowClockTimeNumSelect.Location = new Point(168, 49);
+            ShowClockTimeNumSelect.Location = new Point(131, 42);
+            ShowClockTimeNumSelect.Margin = new Padding(2, 3, 2, 3);
             ShowClockTimeNumSelect.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             ShowClockTimeNumSelect.Name = "ShowClockTimeNumSelect";
-            ShowClockTimeNumSelect.Size = new Size(150, 27);
+            ShowClockTimeNumSelect.Size = new Size(117, 23);
             ShowClockTimeNumSelect.TabIndex = 2;
             ShowClockTimeNumSelect.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // ShowClockTimeLabel
             // 
             ShowClockTimeLabel.AutoSize = true;
-            ShowClockTimeLabel.Location = new Point(22, 51);
+            ShowClockTimeLabel.Location = new Point(17, 43);
+            ShowClockTimeLabel.Margin = new Padding(2, 0, 2, 0);
             ShowClockTimeLabel.Name = "ShowClockTimeLabel";
-            ShowClockTimeLabel.Size = new Size(114, 20);
+            ShowClockTimeLabel.Size = new Size(92, 17);
             ShowClockTimeLabel.TabIndex = 3;
             ShowClockTimeLabel.Text = "弹出时间（秒）";
             // 
             // MinuteIntervalLabel
             // 
             MinuteIntervalLabel.AutoSize = true;
-            MinuteIntervalLabel.Location = new Point(22, 84);
+            MinuteIntervalLabel.Location = new Point(17, 71);
+            MinuteIntervalLabel.Margin = new Padding(2, 0, 2, 0);
             MinuteIntervalLabel.Name = "MinuteIntervalLabel";
-            MinuteIntervalLabel.Size = new Size(129, 20);
+            MinuteIntervalLabel.Size = new Size(104, 17);
             MinuteIntervalLabel.TabIndex = 5;
             MinuteIntervalLabel.Text = "弹出间隔（分钟）";
             // 
             // MinuteIntervalNumSelect
             // 
-            MinuteIntervalNumSelect.Location = new Point(168, 82);
+            MinuteIntervalNumSelect.Location = new Point(131, 70);
+            MinuteIntervalNumSelect.Margin = new Padding(2, 3, 2, 3);
             MinuteIntervalNumSelect.Maximum = new decimal(new int[] { 60, 0, 0, 0 });
             MinuteIntervalNumSelect.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             MinuteIntervalNumSelect.Name = "MinuteIntervalNumSelect";
-            MinuteIntervalNumSelect.Size = new Size(150, 27);
+            MinuteIntervalNumSelect.Size = new Size(117, 23);
             MinuteIntervalNumSelect.TabIndex = 4;
             MinuteIntervalNumSelect.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // SaveButton
             // 
-            SaveButton.Location = new Point(22, 409);
+            SaveButton.Location = new Point(17, 348);
+            SaveButton.Margin = new Padding(2, 3, 2, 3);
             SaveButton.Name = "SaveButton";
-            SaveButton.Size = new Size(94, 29);
+            SaveButton.Size = new Size(73, 25);
             SaveButton.TabIndex = 6;
             SaveButton.Text = "保存";
             SaveButton.UseVisualStyleBackColor = true;
@@ -110,9 +117,10 @@
             // 
             // AbortChangeButton
             // 
-            AbortChangeButton.Location = new Point(122, 409);
+            AbortChangeButton.Location = new Point(95, 348);
+            AbortChangeButton.Margin = new Padding(2, 3, 2, 3);
             AbortChangeButton.Name = "AbortChangeButton";
-            AbortChangeButton.Size = new Size(94, 29);
+            AbortChangeButton.Size = new Size(73, 25);
             AbortChangeButton.TabIndex = 7;
             AbortChangeButton.Text = "取消修改";
             AbortChangeButton.UseVisualStyleBackColor = true;
@@ -121,9 +129,10 @@
             // EnableClockBox
             // 
             EnableClockBox.AutoSize = true;
-            EnableClockBox.Location = new Point(168, 115);
+            EnableClockBox.Location = new Point(131, 98);
+            EnableClockBox.Margin = new Padding(2, 3, 2, 3);
             EnableClockBox.Name = "EnableClockBox";
-            EnableClockBox.Size = new Size(106, 24);
+            EnableClockBox.Size = new Size(87, 21);
             EnableClockBox.TabIndex = 8;
             EnableClockBox.Text = "勾选以启用";
             EnableClockBox.UseVisualStyleBackColor = true;
@@ -131,17 +140,18 @@
             // EnableClockLabel
             // 
             EnableClockLabel.AutoSize = true;
-            EnableClockLabel.Location = new Point(22, 116);
+            EnableClockLabel.Location = new Point(17, 99);
+            EnableClockLabel.Margin = new Padding(2, 0, 2, 0);
             EnableClockLabel.Name = "EnableClockLabel";
-            EnableClockLabel.Size = new Size(69, 20);
+            EnableClockLabel.Size = new Size(56, 17);
             EnableClockLabel.TabIndex = 9;
             EnableClockLabel.Text = "是否启用";
             // 
             // SettingsForm
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(409, 450);
+            ClientSize = new Size(318, 382);
             Controls.Add(EnableClockLabel);
             Controls.Add(EnableClockBox);
             Controls.Add(AbortChangeButton);
@@ -152,8 +162,11 @@
             Controls.Add(ShowClockTimeNumSelect);
             Controls.Add(ShowClockButton);
             Controls.Add(ShowClockLabel);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "SettingsForm";
             Text = "SettingsForm";
+            Load += SettingsForm_Load;
+            Resize += SettingsForm_Resize;
             ((System.ComponentModel.ISupportInitialize)ShowClockTimeNumSelect).EndInit();
             ((System.ComponentModel.ISupportInitialize)MinuteIntervalNumSelect).EndInit();
             ResumeLayout(false);
